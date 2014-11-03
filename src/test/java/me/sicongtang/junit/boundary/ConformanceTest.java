ee1495d45c9c26eb504fb9632bd823f0e5487b95
sicongtang <bobby3239@gmail.com>
2014-11-03 09:30:23 +0800

    update junit case

---
+++ src/test/java/me/sicongtang/junit/boundary/ConformanceTest.java
@@ -0,0 +1,14 @@
+package me.sicongtang.junit.boundary;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConformanceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/me/sicongtang/junit/boundary/ReferenceTest.java
@@ -0,0 +1,14 @@
+package me.sicongtang.junit.boundary;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReferenceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
