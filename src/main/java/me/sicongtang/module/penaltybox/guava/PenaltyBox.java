74ae56475cbacb2ec640b44c317e35a8a27b6322
BobbyTang <bobby3239@gmail.com>
2014-11-27 01:18:56 +0800

    2level cache implementation

--- src/main/java/me/sicongtang/module/penaltybox/guava/PenaltyBox.java
+++ src/main/java/me/sicongtang/module/penaltybox/guava/PenaltyBox.java
@@ -2,7 +2,6 @@ package me.sicongtang.module.penaltybox.guava;
 
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
@@ -21,22 +20,33 @@ public class PenaltyBox {
 
 	private static LoadingCache<String, StatisticsInfo> cache;
 
+	private static LoadingCache<String, StatisticsInfo> penaltyCache;
+
 	private PenaltyBox() {
 		config = new PenaltyConfig();
 
-		cache = CacheBuilder.newBuilder().maximumSize(config.getMaxSize())
-				.expireAfterWrite(config.getExpireSecs(), TimeUnit.SECONDS)
+		cache = CacheBuilder.newBuilder().maximumSize(config.getCacheMaxSize())
+				.expireAfterWrite(config.getCacheExpireSecs(), TimeUnit.SECONDS)
 				.build(new CacheLoader<String, StatisticsInfo>() {
 					public StatisticsInfo load(String key) throws Exception {
 						return new StatisticsInfo();
 					}
 				});
 
-		logger.info("Initialized penalty box[maxSize=" + config.getMaxSize() + ", expireSecs=" + config.getExpireSecs()
-				+ "]");
+		penaltyCache = CacheBuilder.newBuilder().maximumSize(config.getPenaltyMaxSize())
+				.expireAfterWrite(config.getPenaltyExpireSecs(), TimeUnit.SECONDS)
+				.build(new CacheLoader<String, StatisticsInfo>() {
+					public StatisticsInfo load(String key) throws Exception {
+						return new StatisticsInfo();
+					}
+				});
+
+		logger.info("Initialized penalty box cache[maxSize=" + config.getCacheMaxSize() + ", expireSecs="
+				+ config.getCacheExpireSecs() + "], penalty cache[maxSize=" + config.getPenaltyMaxSize()
+				+ ", expireSecs=" + config.getPenaltyExpireSecs() + "]");
 
 		Thread thread = new Thread(new StatThread());
-		logger.info("Start statistics thread in order to print penalty items.");
+		logger.info("Start statistics thread in order to print penalty items");
 		thread.start();
 
 	}
@@ -45,22 +55,39 @@ public class PenaltyBox {
 		return box;
 	}
 
-	public LoadingCache<String, StatisticsInfo> getLoadingCache() {
+	public LoadingCache<String, StatisticsInfo> getCache() {
 		return cache;
+	}
+
+	public LoadingCache<String, StatisticsInfo> getPenaltyCache() {
+		return penaltyCache;
 	}
 
 	private synchronized StatisticsInfo record(String key) {
 		StatisticsInfo info = null;
-		try {
-			info = cache.get(key);
+
+		// check penalty cache
+		info = penaltyCache.getIfPresent(key);
+		if (info != null) {
 			info.increaseCount();
-			// only below threshold can be put into cache
-			if (info.getCount() <= config.getThreshold()) {
-				cache.put(key, info);
-			}
-		} catch (ExecutionException e) {
-			logger.error("Encounter error while geting key from cache.", e);
+
+			return info;
 		}
+
+		// check cache
+		info = cache.getIfPresent(key);
+		if (info == null) {
+			info = new StatisticsInfo();
+		}
+		info.increaseCount();
+
+		if (info.getCount() <= config.getThreshold()) {
+			cache.put(key, info);
+		} else {
+			penaltyCache.put(key, info);
+			cache.invalidate(key);
+		}
+
 		return info;
 	}
 
@@ -80,15 +107,6 @@ public class PenaltyBox {
 		StatisticsInfo info = null;
 		info = record(key);
 
-		// get count
-		if (info == null || info.getCount() <= config.getThreshold()) {
-			try {
-				info = cache.get(key);
-			} catch (ExecutionException e) {
-				logger.error("Encounter error while geting key from cache.", e);
-			}
-		}
-
 		// check if great than threshold
 		if (info.getCount() > config.getThreshold()) {
 			bool = true;
@@ -103,20 +121,21 @@ public class PenaltyBox {
 		public void run() {
 			StringBuilder builder = new StringBuilder();
 			while (true) {
-				Set<Entry<String, StatisticsInfo>> entrySet = cache.asMap().entrySet();
+				Set<Entry<String, StatisticsInfo>> entrySet = penaltyCache.asMap().entrySet();
 				logger.info("The total number of penalty box element is " + entrySet.size());
 				int count = 0;
 
 				builder.delete(0, builder.length());
 
 				for (Entry<String, StatisticsInfo> entry : entrySet) {
-					if (entry.getValue().getCount() > config.getThreshold()) {
-						count++;
-						builder.append(entry.getKey());
-						builder.append("=");
-						builder.append(entry.getValue());
-						builder.append("|");
-					}
+					// if (entry.getValue().getCount() > config.getThreshold())
+					// {
+					count++;
+					builder.append(entry.getKey());
+					builder.append("=");
+					builder.append(entry.getValue());
+					builder.append("|");
+					// }
 				}
 
 				logger.info("The number of penalty box element that exceeds threshold[=" + config.getThreshold()
--- src/main/java/me/sicongtang/module/penaltybox/guava/PenaltyConfig.java
+++ src/main/java/me/sicongtang/module/penaltybox/guava/PenaltyConfig.java
@@ -4,36 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PenaltyConfig {
-	private long maxSize;
-	private long expireSecs;
+	private long cacheMaxSize;
+	private long cacheExpireSecs;
 	private long threshold;
 	private List<String> excludeKeys;
 	private boolean disabledFlag;
 	private long printInterval;
+	private long penaltyMaxSize;
+	private long penaltyExpireSecs;
 
 	public PenaltyConfig() {
-		// default as 10000 size
-		this.maxSize = 5;
-		// default as 1 hour - 60 * 60 * 1
-		this.expireSecs = 30;
-		// default as 20 access - 20
-		this.threshold = 1;
+		this.cacheMaxSize = 5; // default as 10000 size
+		this.cacheExpireSecs = 30; // default as 1 hour - 60 * 60 * 1
+		
+		this.threshold = 1; // default as 20 access - 20
 		this.excludeKeys = new ArrayList<String>();
 		this.disabledFlag = false;
-		// default as 5 mins - 60 * 5
-		this.printInterval = 10;
+		
+		this.printInterval = 10; // default as 5 mins - 60 * 5
+		this.penaltyMaxSize = 5; // default as 5000 size
+		this.penaltyExpireSecs = 60; // default as 2 hour - 60 * 60 * 1
 	}
 
 	public boolean isDisabled() {
 		return disabledFlag;
 	}
 
-	public long getMaxSize() {
-		return maxSize;
+	public long getCacheMaxSize() {
+		return cacheMaxSize;
 	}
 
-	public long getExpireSecs() {
-		return expireSecs;
+	public long getCacheExpireSecs() {
+		return cacheExpireSecs;
 	}
 
 	public long getThreshold() {
@@ -47,4 +49,14 @@ public class PenaltyConfig {
 	public long getPrintInterval() {
 		return printInterval;
 	}
+
+	public long getPenaltyMaxSize() {
+		return penaltyMaxSize;
+	}
+
+	public long getPenaltyExpireSecs() {
+		return penaltyExpireSecs;
+	}
+
+
 }
