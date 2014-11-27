f86c18d275c2898e18bb309e9779aac58774a6a4
BobbyTang <bobby3239@gmail.com>
2014-11-27 19:38:49 +0800

    update penalty box

--- src/test/java/me/sicongtang/module/penaltybox/guava/PenaltyBoxExpireTest.java
+++ src/test/java/me/sicongtang/module/penaltybox/guava/PenaltyBoxExpireTest.java
@@ -1,26 +1,45 @@
 package me.sicongtang.module.penaltybox.guava;
 
-
 public class PenaltyBoxExpireTest {
 
 	public static void main(String[] args) {
 		final PenaltyBox box = PenaltyBox.getInstance();
 
 		String[] ips = { "191.168.1.1", "191.168.1.2", "191.168.1.3", "191.168.1.4", "191.168.1.5" };
-		for(final String ip: ips) {
+		for (final String ip : ips) {
 			Thread thread = new Thread(new Runnable() {
 				@Override
 				public void run() {
-					while(true){
+					for (int i = 0; i < 20; i++) {
 						box.isPenalty(ip);
-						
+
 						try {
 							Thread.sleep(300);
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
 					}
+
+					System.out.println("=====1=====");
 					
+					try {
+						Thread.sleep(60000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					
+					System.out.println("=====2=====");
+					
+					for (int i = 0; i < 1; i++) {
+						box.isPenalty(ip);
+
+						try {
+							Thread.sleep(300);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+
 				}
 			});
 			thread.start();
