e5a454f0aa4cbafe17c8d905666ead952d671cb4
BobbyTang <bobby3239@gmail.com>
2014-12-03 23:18:54 +0800

    update mockito basic example

--- src/test/java/me/sicongtang/junit/mockito/BasicMockito.java
+++ src/test/java/me/sicongtang/junit/mockito/BasicMockito.java
@@ -1,13 +1,13 @@
 package me.sicongtang.junit.mockito;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
 
 import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.Test;
+import org.mockito.InOrder;
 
 public class BasicMockito {
 
@@ -67,18 +67,20 @@ public class BasicMockito {
 	 */
 	@Test
 	public void testArgumentMatchers() {
-		// // stubbing using built-in anyInt() argument matcher
-		// when(mockedList.get(anyInt())).thenReturn("element");
-		//
-		// // stubbing using hamcrest (let's say isValid() returns your own
-		// // hamcrest matcher):
+		LinkedList mockedList = mock(LinkedList.class);
+
+		// stubbing using built-in anyInt() argument matcher
+		when(mockedList.get(anyInt())).thenReturn("element");
+
+		// stubbing using hamcrest (let's say isValid() returns your own
+		// hamcrest matcher):
 		// when(mockedList.contains(argThat(isValid()))).thenReturn("element");
-		//
-		// // following prints "element"
-		// System.out.println(mockedList.get(999));
-		//
-		// // you can also verify using an argument matcher
-		// verify(mockedList).get(anyInt());
+
+		// following prints "element"
+		System.out.println(mockedList.get(999));
+
+		// you can also verify using an argument matcher
+		verify(mockedList).get(anyInt());
 
 	}
 
@@ -87,6 +89,123 @@ public class BasicMockito {
 	 */
 	@Test
 	public void testVerifyInvocations() {
+		LinkedList mockedList = mock(LinkedList.class);
+		// using mock
+		mockedList.add("once");
 
+		mockedList.add("twice");
+		mockedList.add("twice");
+
+		mockedList.add("three times");
+		mockedList.add("three times");
+		mockedList.add("three times");
+
+		// following two verifications work exactly the same - times(1) is used
+		// by default
+		verify(mockedList).add("once");
+		verify(mockedList, times(1)).add("once");
+
+		// exact number of invocations verification
+		verify(mockedList, times(2)).add("twice");
+		verify(mockedList, times(3)).add("three times");
+
+		// verification using never(). never() is an alias to times(0)
+		verify(mockedList, never()).add("never happened");
+
+		// verification using atLeast()/atMost()
+		verify(mockedList, atLeastOnce()).add("three times");
+		verify(mockedList, atLeast(2)).add("five times");
+		verify(mockedList, atMost(5)).add("three times");
+	}
+
+	/**
+	 * 5. Stubbing void methods with exceptions
+	 */
+	@Test
+	public void testStubbingVoidMethodsWithExceptions() {
+		LinkedList mockedList = mock(LinkedList.class);
+		doThrow(new RuntimeException()).when(mockedList).clear();
+
+		// following throws RuntimeException:
+		mockedList.clear();
+	}
+
+	/**
+	 * 6. Verification in order
+	 * 
+	 */
+	@Test
+	public void testVerificationInOrder() {
+		List firstMock = mock(List.class);
+		List secondMock = mock(List.class);
+
+		// using mocks
+		firstMock.add("was called first");
+		secondMock.add("was called second");
+
+		// create inOrder object passing any mocks that need to be verified in
+		// order
+		InOrder inOrder = inOrder(firstMock, secondMock);
+
+		// following will make sure that firstMock was called before secondMock
+		inOrder.verify(firstMock).add("was called first");
+		inOrder.verify(secondMock).add("was called second");
+	}
+
+	/**
+	 * 7. Making sure interaction(s) never happened on mock
+	 */
+	@Test
+	public void testInteractionNeverHappenedOnMock() {
+		List mockOne = mock(List.class);
+		List mockTwo = mock(List.class);
+		List mockThree = mock(List.class);
+		// using mocks - only mockOne is interacted
+		mockOne.add("one");
+
+		// ordinary verification
+		verify(mockOne).add("one");
+
+		// verify that method was never called on a mock
+		verify(mockOne, never()).add("two");
+
+		// verify that other mocks were not interacted
+		verifyZeroInteractions(mockTwo, mockThree);
+
+	}
+
+	/**
+	 * 8. Finding redundant invocations
+	 */
+	@Test
+	public void testFindRedundantInvocations() {
+		List mockedList = mock(List.class);
+
+		// using mocks
+		mockedList.add("one");
+		mockedList.add("two");
+
+		verify(mockedList).add("one");
+
+		// following verification will fail
+		verifyNoMoreInteractions(mockedList);
+	}
+
+	/**
+	 * 10. Stubbing consecutive calls (iterator-style stubbing)
+	 */
+	@Test
+	public void testStubbingConsecutiveCalls() {
+		
+//		when(mock.someMethod("some arg")).thenThrow(new RuntimeException()).thenReturn("foo");
+//
+//		// First call: throws runtime exception:
+//		mock.someMethod("some arg");
+//
+//		// Second call: prints "foo"
+//		System.out.println(mock.someMethod("some arg"));
+//
+//		// Any consecutive call: prints "foo" as well (last stubbing wins).
+//		System.out.println(mock.someMethod("some arg"));
 	}
 }
